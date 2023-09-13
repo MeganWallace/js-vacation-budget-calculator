0c18cb13da233d1512c492c9a30bcf2297432227
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-13 00:16:47 -0400

    Spelling correction

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var dailyBudget = document.querySelector(".daily-budget");
 
 var tripInfo = function(){
   var totalBudget=Number(prompt('What is your total budget?'));
-  var accomodation = Number(prompt('What are your accommodation costs?'));
+  var accommodation = Number(prompt('What are your accommodation costs?'));
   var numDays = Number(prompt('How many days does your trip last?'));
 };
 
